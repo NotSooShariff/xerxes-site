ab5d215b109b6d015e465287b3ebe13ffa28af71
Owais Shariff <osh.econnect@gmail>
2023-12-13 21:29:53 +0300

    Rotate Animation Added to Tweet

--- src/components/tweetsticker/Tweet.jsx
+++ src/components/tweetsticker/Tweet.jsx
@@ -4,7 +4,7 @@ import RefImage from './tweet-reference.png'
 
 const Tweet = () => {
   return (
-    <div class="-rotate-3 px-8 md:px-16 py-8 md:py-16 flex justify-center items-center">
+    <div class="-rotate-3 hover:rotate-3 transition-all duration-500 px-8 md:px-16 py-8 md:py-16 flex justify-center items-center">
       <Image src="/tweetsticker.png" width={800} height={594}/>
     </div>
 
