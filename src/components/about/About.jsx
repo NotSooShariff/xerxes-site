264589a9dd94e963ea75ecc3659e9ab607c66bb9
Owais Shariff <osh.econnect@gmail>
2023-12-14 01:15:17 +0300

    UI Bug Fix

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,13 +5,13 @@ import RefImage from './about-reference.png'
 const About = () => {
   return (
     <div class='py-16 lg:pl-60'>
-        <h2 class='tracking-widest text-3xl lg:text-5xl lg:pl-16 lg:pb-8'>
+        <h2 class='tracking-widest text-center lg:text-left text-3xl lg:text-5xl lg:pl-16 lg:pb-8'>
           THE ENCHANTED
         </h2>
-        <h1 class='tracking-widest text-5xl pl-2 lg:text-8xl'>
+        <h1 class='tracking-widest text-center lg:text-left text-5xl pl-2 lg:text-8xl'>
           AMPHIBIAN
         </h1>
-        <h1 class='tracking-widest text-5xl pl-2 lg:pt-10 lg:pl-32 lg:text-8xl'>
+        <h1 class='tracking-widest text-center lg:text-left pl-2 lg:pt-10 lg:pl-32 lg:text-8xl'>
           COMPANION
         </h1>
         <Image src={"/ownedbygrok.png"} class="lg:pt-10 mx-auto" height={200} width={500} alt='owned by grok'/>
