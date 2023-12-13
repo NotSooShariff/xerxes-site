85e05f3a64048df4c34dcbac5b756a2461aa8888
Owais Shariff <osh.econnect@gmail>
2023-12-14 01:11:29 +0300

    UI Update

--- src/components/venn/Venn.jsx
+++ src/components/venn/Venn.jsx
@@ -8,7 +8,7 @@ const Venn = () => {
   return (
     <div>
       <section class='lg:pl-48'>
-        <div className="flex items-center justify-center flex-col lg:justify-between">
+        <div className="flex justify-between items-center flex-col sm:flex-row lg:flex-row">
         <div className="left">
         <p class='mt-3 text-3xl'>FROGGYNOMICS:   </p>  
         <p class='mt-10 text-3xl'>Tax: 2/2        </p>
@@ -18,14 +18,14 @@ const Venn = () => {
         <p class='mt-3 text-3xl'>SYMBOL: XERXES  </p>
         <p class='mt-3 text-3xl'>DECIMALS: 7     </p>
         </div>
-        <div class='flex my-10 mx-auto'>
+        <div class='flex my-10'>
         <div class="flex flex-col items-center">
           <Image width={150} height={150} src="/byte.png" alt="Byte" class="w-50 h-50 rounded-full mb-2"/>
           <p class="text-3xl text-white">BYTE</p>
           <p class="mt-2 text-2xl text-white">[GROK's <wbr/> DOG]</p>
           <p class="text-2xl text-white">(ATH: 15M+)</p>
         </div>
-        <div class="flex flex-col items-center mx-10 lg:mr-48">
+        <div class="flex flex-col items-center ml-10 lg:mr-48">
           <Image width={150} height={150} src="/xerxes.png" alt="Xerxes" class="w-50 h-50 rounded-full mb-2"/>
           <p class="text-3xl text-white">XERXES</p>
           <p class="mt-2 text-2xl text-white">[GROK'S <wbr/>FROG]</p>
